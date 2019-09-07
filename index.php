c8bbf9ef911dfca32496c9d00afb364e10fa3daa
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-08 01:56:58 +0200

    added a date input and btn to form

--- index.php
+++ index.php
@@ -4,24 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- title -->
     <title>Hotel Booking</title>
 </head>
 <body>
+    <!-- Form validation -->
     <form action="" method="post">
-        <div><h1>Hotel Booking Form</h1></div>
+    <!-- form heading -->
+        <h1>Hotel Booking Form</h1>
         <h2>First Name:</h2>
-        <input type="text" name="firstName" reguired>
+        <input type="text" name="firstName" required>
         <h2>Last Name:</h2>
-        <input type="text" name="LastName" reguired>
+       <input type="text" name="lastName" required>
 
-        <div><h1>Select your ideal Hotel</h1></div>
+        <!-- Multiple Hotel selection -->
+        <h1>Select your ideal Hotel</h1>
         <select name="hotelSelection" id="">
             <option value="signature lux">Signature Lux Hotel</option>
             <option value="the table bay ">The Table Bay Hotel</option>
             <option value="the tree house">The Tree House Boutique Hotel</option>
             <option value="the grey hotel">The Grey Hotel</option>
         </select>
-        
+
+        <!-- user select date input -->
+        <h2>Select your date:</h2>
+        <input type="date" name="selectDays" min= 1 required>
+        <input type="date" name="selectDays" min= 1 required>
+         <br/>
+         <!-- submit button -->
+        <button type="submit" name="submit">Submit</button>
     </form>
+    <!-- End of form -->
+    <!-- Php tags -->
 </body>
 </html>
