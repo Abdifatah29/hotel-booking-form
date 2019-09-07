adc4d3be177be6613a1415857baa7395cea1cb55
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-07 15:47:21 +0200

    made a basic form

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hotel Booking</title>
+</head>
+<body>
+    <form action="" method="post">
+        <div><h1>Hotel Booking Form</h1></div>
+        <h2>First Name:</h2>
+        <input type="text" name="firstName" reguired>
+        <h2>Last Name:</h2>
+        <input type="text" name="LastName" reguired>
+
+        <div><h1>Select your ideal Hotel</h1></div>
+        <select name="hotelSelection" id="">
+            <option value="signature lux">Signature Lux Hotel</option>
+            <option value="the table bay ">The Table Bay Hotel</option>
+            <option value="the tree house">The Tree House Boutique Hotel</option>
+            <option value="the grey hotel">The Grey Hotel</option>
+        </select>
+        
+    </form>
+</body>
+</html>
