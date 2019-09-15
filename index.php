1b3fee594655612ca6f705dfdf7a322f03530c84
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-16 00:57:54 +0200

    returned user info dynamically

--- index.php
+++ index.php
@@ -7,10 +7,21 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- title -->
     <title>Hotel Booking</title>
+    <!-- bootstap link -->
+    <link 
+        rel="stylesheet" 
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+        crossorigin="anonymous">
+    
 </head>
 <body>
+    <div>
+
+    </div>
+    <main class="container banner">
     <!-- Form validation -->
-    <form action="" method="post">
+    <form action="" method="post" class="formContent">
     <!-- form heading -->
         <h1>Hotel Booking Form</h1>
         <h2>First Name:</h2>
@@ -23,55 +34,83 @@
         <select name="hotelSelection" id="hotelSelection">
 		    <option>Select hotel</option>
             <option value="signatureLux">Signature Lux Hotel</option>
-            <option value="tableBay">The Table Bay Hotel</option>
+            <option value="tableBay" >The Table Bay Hotel</option>
             <option value="treeHouse">The Tree House Boutique Hotel</option>
             <option value="greyHotel">The Grey Hotel</option>
         </select>
-
+        <!-- displaying hotel description and image -->
         <div class='hotel-info'>
             <div id='hotel-image' class='hotel-image'></div>
-            <div id='hotel-details' class='hotel-details'></div>
+            <div class="selectedInfo">
+                <div id='hotel-details' class='hotel-details'></div>
+                <div id='hotel-price' class='hotel-details'></div>
+                <div id='hotel-rating' class='hotel-details'></div>
+                <div id='hotel-describtion' class='hotel-details'></div>
+            </div>
         </div>
 
         <!-- user select date input -->
         <h2>Select your date:</h2>
-        <input type="date" name="selectDays" min= 1 required>
-        <input type="date" name="selectDays" min= 1 required>
+        <input type="date" name="dateCheckedIn" min= 1 required>
+        <input type="date" name="dateCheckedOut" min= 1 required>
          <br/>
          <!-- submit button -->
         <button type="submit" name="submit">Submit</button>
     </form>
+    <?php
+    // getting user input from the form
+        if (isset($_POST['submit'])) {
+            //storing the input inside a variable
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $hotelSelected = $_POST['hotelSelection'];
+            $dateCheckedIn = $_POST['dateCheckedIn'];
+            $dateCheckedOut = $_POST['dateCheckedOut'];
+            $days_between = ceil(abs(strtotime($dateCheckedOut) - strtotime($dateCheckedIn)) / 86400) + 1;
+            $price = 0;
+            
+            // setting the price for which ever hotel users select
+            if ($hotelSelected == "signatureLux" ) {
+                $price =  900;
+            } else if($hotelSelected == "tableBay" ){
+                $price = 1200;
+            } else if($hotelSelected == "treeHouse" ){
+                $price = 800;
+            } else if($hotelSelected == "greyHotel" && $price < 1){
+                $price =  750;
+            } else {
+                echo "Please Select a hotel";
+            }
+            
+            
+            //Echo out the results for user selections
+            echo '<div class="bookCheckin"> 
+                    <p>Name: '. $firstName . '</P>';
+            echo "<p>Last Name: " . $lastName ."</br>"; 
+            echo "<p>Hotel Selected: " . $hotelSelected .'</P>';
+            echo "<p>Date Check In: ". $dateCheckedIn .'</P>'; 
+            echo "<p>Date Check Out: ". $dateCheckedOut.'</P>'; 
+            echo "<p>Number of Days: ". $days_between.'</P>'; 
+            echo "<p>Total Price: ". $price * $days_between; 
+            
+            // echo "asd: ". $priceOne * $days_between; 
+                
+
+        }
+        
+            
+    ?>
+    </main>
     <!-- End of form -->
     <!-- Php tags -->
-    <script type='text/javascript' >
-        	let hotels = {
-		signatureLux: {
-			name: 'Signature Lux Hotel',
-			image: 'http://placehold.it/400x400'
-		},
 
-		tableBay: {
-			name: 'The Table Bay Hotel',
-			image: 'http://placehold.it/400x400'
-		},
-
-		treeHouse: {
-			name: 'The Tree House Boutique Hotel',
-			image: 'http://placehold.it/400x400'
-		},
-
-		greyHotel: {
-			name: 'The Grey Hotelr',
-			image: 'http://placehold.it/400x400'
-		}
-	};
-
-	let hotelSelection = document.getElementById('hotelSelection')
-	hotelSelection.addEventListener('change', function() {
-		let selectedHotel = hotels[hotelSelection.options[hotelSelection.selectedIndex].value];
-		document.getElementById('hotel-image').innerHTML = "<img src=" + selectedHotel.image + "/>";
-		document.getElementById('hotel-details').innerHTML = selectedHotel.name;
-	});
-    </script>
+    <!-- scripts -->
+    <!-- Main js Scripts -->
+    <script src="js/main.js" type='text/javascript' ></script>
+    <!-- bootstrap scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- End of scripts -->
 </body>
 </html>
