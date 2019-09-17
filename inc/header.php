99bc35e73a578f0bea660dbfcb04978a8d418d39
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-17 21:56:53 +0200

    took out the landing page

--- inc/header.php
+++ inc/header.php
@@ -13,59 +13,9 @@
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
         crossorigin="anonymous">
-    
 </head>
 <body>
-    <!-- main Navigation -->
-    <header>
-        <!--Navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
-        <div class="container">
-            <a class="navbar-brand" href="#"><strong>MDB</strong></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Profile</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
-        <!-- Navbar -->
-        <!-- Full Page Intro -->
-        <div class="view">
-        <!-- Mask & flexbox options-->
-        <div class="mask rgba-black-light align-items-center">
-            <!-- Content -->
-            <div class="container">
-            <!--Grid row-->
-            <div class="row">
-                <!--Grid column-->
-                <div class="col-md-12 mb-4 white-text text-center landingInfo">
-                <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay="0.3s"><strong>The world is yours</strong></h1>
-                <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
-                <h5 class="text-uppercase mb-4 white-text wow fadeInDown" data-wow-delay="0.4s"><strong>Just Go</strong></h5>
-                </div>
-                <!--Grid column-->
-            </div>
-            <!--Grid row-->
-            </div>
-            <!-- Content -->
-        </div>
-        <!-- Mask & flexbox options-->
-        </div>
-        <!-- Full Page Intro -->
-    </header>
-    <!-- Main navigation -->
+   <!-- main container -->
     <main class=" banner">
     <!-- Form validation -->
     <form action="" method="post" class="formContent">
@@ -97,7 +47,6 @@
                 <div id='hotel-describtion' class='hotel-details'></div>
             </div>
         </div>
-
         <!-- user select date input -->
         <h2>Select your date:</h2>
         <input type="date" name="dateCheckedIn" min= 1 required>
