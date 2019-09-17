35415c32fc6bb2a81568ba8f1997257e67b3cdf5
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-18 01:07:15 +0200

    styled the form

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- title -->
     <title>Hotel Booking</title>
+    <!-- google font for headings -->
+    <link href="https://fonts.googleapis.com/css?family=Gayathri&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <!-- bootstap link -->
     <link 
         rel="stylesheet" 
@@ -20,16 +23,14 @@
     <!-- Form validation -->
     <form action="" method="post" class="formContent">
     <!-- form heading -->
-        <h1>Hotel Booking Form</h1>
+        <h1  class="article__title">Hotel Booking</h1>
         <h2>First Name:</h2>
-        <input type="text" name="firstName" required class="form-control md-form">
-        <small id="nameHelp" class="form-text text-muted">We'll never share your Name with anyone else.</small>
+        <input type="text" name="firstName" required class="inputText" placeholder="Your name...">
         <h2>Last Name:</h2>
-       <input type="text" name="lastName" required  class="form-control">
-        <small id="lastHelp" class="form-text text-muted">We'll never share your Surname with anyone else.</small>
+       <input type="text" name="lastName" required  class="inputText" placeholder="Your last name...">
 
-        <!-- Multiple Hotel selection -->
-        <h1>Select your ideal Hotel</h1>
+       <!-- Multiple Hotel selection -->
+        <h1 class="article__title">Select your ideal Hotel</h1>
         <select name="hotelSelection" id="hotelSelection">
 		    <option>Select hotel</option>
             <option value="signatureLux">Signature Lux Hotel</option>
@@ -41,14 +42,14 @@
         <div class='hotel-info '>
             <div id='imageOne' class='hotel-image '></div>
             <div class="selectedInfo ">
-                <div id='hotel-details' class='hotel-details'></div>
-                <div id='hotel-price' class='hotel-details'></div>
-                <div id='hotel-rating' class='hotel-details'></div>
-                <div id='hotel-describtion' class='hotel-details'></div>
+                <div id='hotel-details' class='hotel-details article__title'></div>
+                <div id='hotel-price' class='hotel-details article__title'></div>
+                <div id='hotel-rating' class='hotel-details article__title'></div>
+                <div id='hotel-describtion' class='hotel-details article__content'></div>
             </div>
         </div>
         <!-- user select date input -->
-        <h2>Select your date:</h2>
+        <h2 class="article__title">Select your date:</h2>
         <input type="date" name="dateCheckedIn" min= 1 required>
         <input type="date" name="dateCheckedOut" min= 1 required>
          <br/>
