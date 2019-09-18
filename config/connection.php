ed331498379fd6519d4f8c28fa6318e2e41b5e21
Abdifatah29 <abdi.hassannoor01@gmail.com>
2019-09-19 00:10:34 +0200

    fixed indentaion and commented

--- config/connection.php
+++ config/connection.php
@@ -1,12 +1,12 @@
 <?php
 // including user creds
-     include 'base.php';
+include 'base.php';
     //create connection 
-    $conn = new mysqli($serverName,$userName,$userPassword, $dbName);
+$conn = new mysqli($serverName,$userName,$userPassword, $dbName);
 
     //testing if connection is sucessful or failed
-    if ($conn->connect_error) {
-        die("Connection not available:". $conn->connect_error);
-    }
+if ($conn->connect_error) {
+	die("Connection not available:". $conn->connect_error);
+}
 
 
